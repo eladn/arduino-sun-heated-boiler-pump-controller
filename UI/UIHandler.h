89cb27a8efb0d5b1381f59c5039b1ac3bb74d83f
Elad Nachmias <eladnah@gmail.com>
2018-04-15 04:56:18 +0300

    UIHandler - init
    lacks full implementation.
    does not know yet to have modes and switch these.

---
+++ UI/UIHandler.h
@@ -0,0 +1,75 @@
+#ifndef UI_HANDLER_H_
+#define UI_HANDLER_H_
+
+#define UI_MAX_BUTTONS 8
+
+#include "UIButton.h"
+#include "UIScreenModeInterface.h"
+
+
+class UIHandler {
+public:
+	typedef int ButtonIdx;
+	typedef UIButton<UIHandler, ButtonIdx, 1> ButtonType;
+	typedef ButtonType::ProxyType ButtonProxyType;
+	typedef UIButton<UIHandler, void*, 1> ModeButtonType;
+	typedef ModeButtonType::ProxyType ModeButtonProxyType;
+	
+private:
+	ModeButtonType modeButton;
+	
+	// TODO: array of buttons!
+	struct ButtonDetails {
+		ButtonType button;
+		ButtonIdx buttonIdx;
+		
+		ButtonDetails() : button(0), buttonIdx() {}
+	} buttons[UI_MAX_BUTTONS];
+	int nrButtons;
+	
+	UIScreenModeInterfaceBase *curScreenMode;
+public:
+	// TODO: implement!
+	
+	UIHandler(int modeButtonPin)
+		: modeButton(modeButtonPin),
+		nrButtons(0)
+	{
+		this->modeButton.onClick(ModeButtonProxyType(this, &UIHandler::switchToNextMode), NULL);
+	}
+	
+	// Notice: Must NOT be called after init() has been called!
+	void setButton(int buttonPin, ButtonIdx buttonIdx) {
+		this->buttons[this->nrButtons].button = ButtonType(buttonPin);
+		this->buttons[this->nrButtons].button.registerHandler(ButtonProxyType(this, &UIHandler::buttonEventsOccurred), buttonIdx);
+		this->buttons[this->nrButtons].buttonIdx = buttonIdx;
+		this->nrButtons++;
+	}
+	
+	void switchToNextMode(unsigned int, void*) {
+		assert(curScreenMode != NULL);
+		
+		// TODO: implement!
+		
+		//this->curScreenMode->swichedOut();
+		//this->curScreenMode = this->curScreenMode->nextMode;
+		//this->curScreenMode->swichedIn();
+	}
+	
+	void buttonEventsOccurred(unsigned int events, ButtonIdx buttonIdx) {
+		this->curScreenMode->buttonEventsOccurred(events, buttonIdx);
+	}
+	
+	void init() {
+		this->modeButton.init();
+		
+		for (int i = 0; i < this->nrButtons; ++i) {
+			this->buttons[i].button.init();
+		}
+		
+		// TODO: init modes!
+	}
+	
+};
+
+#endif /* UI_HANDLER_H_ */
